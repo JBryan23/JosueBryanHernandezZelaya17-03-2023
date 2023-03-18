b02f9f6d0e47270cbbeeb38af11f44f19c155442
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-03-17 22:08:46 -0600

    Fin del programa

---
+++ JosueBryanHernandezZelaya17032023/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using JosueBryanHernandezZelaya17032023.Models;
+
+using AlmacenBdContext BaseDeDatos = new AlmacenBdContext();
+
+Producto product = new Producto();
+
+Console.Write("Porfavor ingrese el nombre del producto: ");
+product.Nombre = Console.ReadLine();
+
+Console.Write("Porfavor escriba una breve descripcion del producto: ");
+product.Descripcion = Console.ReadLine();
+
+Console.Write("Porfavor ingrese el precio del producto: ");
+product.Precio = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Porfavor ingrese la cantidad de productos existentes: ");
+product.Stock = Convert.ToInt32(Console.ReadLine());
+
+BaseDeDatos.Productos.Add(product);
+BaseDeDatos.SaveChanges();
+
+var ListProductos =  BaseDeDatos.Productos.ToList();
+
+foreach (var items in ListProductos)
+{
+    Console.WriteLine($"\nNombre asignado:  {items.Nombre}");
+    Console.WriteLine($"Descripcion asignado:  {items.Descripcion}");
+    Console.WriteLine($"Precio asignado:  {items.Precio}");
+    Console.WriteLine($"Stock asignado:  {items.Stock}");
+
+}
+
+
+
+
+
